6480eecb0536abbc48fb3f24291b0d6883257ffb
Jessica <2461291608@qq.com>
2021-08-07 20:42:03 +0800

    my first commit

---
+++ src/Sen.java
@@ -0,0 +1,44 @@
+public class Sen {
+    public static void main(String[]args){
+
+
+
+        int gao =3000;
+       final double PI = 3.14;
+       double r=4;
+       double area =PI*r*r;
+       double circle = 2*PI*r;
+       System.out.println("area="+area);
+       System.out.println("circle="+circle);
+       int $a=2;
+        int b = 015;
+        int c = 0x15;
+        int d = 0b1101;
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+        System.out.println("d="+d);
+        byte age = 30;
+        short salary = 30000;
+        int population = 2000000000;
+        long globalPopulation = 7400000000000000000L;
+        float a = 3.14F;
+        double e = 6.28;
+        double f = 628E-2;
+        System.out.println(f);
+        float i =0.1F;
+        double j =1.0/10;
+        System.out.println(i==j);
+        char k = 'T';
+        char l = 's';
+        char m = '\u0061';
+        System.out.println(m);
+        String n = "abc";
+        boolean man =true;
+        if(man){
+            System.out.println("男性");
+        }
+        System.out.println(""+'a'+'\''+'b');
+        String o = "abc";
+        System.out.println(o);
+    }
+}
